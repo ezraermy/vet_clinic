f8ecf9f1d72b5b654c63080ff381bf4525cf0b21
Ezra <ezraermy@gmail.com>
2023-05-16 20:50:34 +0300

    Improve database performance

--- queries.sql
+++ queries.sql
@@ -151,7 +151,7 @@ ORDER BY visit_count DESC
 LIMIT 1;
 
 EXPLAIN ANALYZE SELECT count(*) FROM visits WHERE animal_id = 4;
-EXPLAIN ANALYZE SELECT * FROM visits where vet_id = 2;
+EXPLAIN ANALYZE SELECT vet_id FROM visits where vet_id = 2;
 EXPLAIN ANALYZE SELECT * FROM owners where email = 'owner_18327@mail.com';
 
 
